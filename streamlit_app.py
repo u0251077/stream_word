04f6cd099ce1007b357eed34154d32333274dae6
Pei Ying <u0251077@gmail.com>
2024-05-21 13:58:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,72 @@
+import streamlit as st
+from pathlib import Path
+from openai import OpenAI
+import openpyxl
+import random
+
+
+
+
+
+def translate(sent, text,api_key):
+    client = OpenAI(api_key=api_key)        
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": "You are asked to translate an English word into Traditional Chinese."},
+            {"role": "user", "content": f"Sentence is {sent} and word is {text}"}
+        ],
+        temperature=0.7,
+        max_tokens=64,
+        top_p=1
+    )
+    return response.choices[0].message.content
+
+
+def translate_sent(sent,api_key):
+    client = OpenAI(api_key=api_key)            
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": "You are asked to translate an English sentence into Traditional Chinese."},
+            {"role": "user", "content": sent}
+        ],
+        temperature=0.7,
+        max_tokens=64,
+        top_p=1
+    )
+    return response.choices[0].message.content
+
+
+def makesentences(word,api_key):
+    client = OpenAI(api_key=api_key)        
+    temperature = random.uniform(0.5, 1.0)
+    variations = ["", ".", "!", "?"]
+    modified_word = word + random.choice(variations)
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": "Create an example sentence using the given English word."},
+            {"role": "user", "content": modified_word}
+        ],
+        temperature=temperature,
+        max_tokens=64,
+        top_p=1
+    )
+    return response.choices[0].message.content
+
+
+def read_excel_words(filename):
+    words = []
+    try:
+        workbook = openpyxl.load_workbook(filename)
+        sheet = workbook.active
+        for row in sheet.iter_rows(values_only=True):
+            words.extend([cell for cell in row if isinstance(cell, str)])
+    except FileNotFoundError:
+        st.error("找不到指定的 Excel 檔案。")
+    return words
 def main():
     with st.sidebar:
         openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
